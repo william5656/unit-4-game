bba3a68c222861f15cc02e14f7292ac7ae936b08
william zhu <william_zhu@hotmail.ca>
2018-06-13 01:14:03 -0400

    adding random number inputs

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,39 @@
 $(document).ready(function() {
+var wins= 0;
+var loss= 0;
+var goal= 0;
+var score = 0;
 
 
+function crystalGame(result){
+var goal = Math.floor(Math.random() * 120) + 30;
+$(".goal").append(goal);
+console.log(goal);
 
 
+var numopt1 = Math.floor(Math.random() * 12) + 1; 
+console.log(numopt1);
 
+var numopt2 = Math.floor(Math.random() * 12) + 1; 
+console.log(numopt2);
 
+var numopt3 = Math.floor(Math.random() * 12) + 1; 
+console.log(numopt3);
 
+var numopt4 = Math.floor(Math.random() * 12) + 1; 
+console.log(numopt4);
 
+$("#crystals1").on("click", function(){
+score = score + numopt;
+console.log(score);
+})
 
+$("#crystals2").on("click", function(){
+    score = score + numopt1;
+    console.log(score);
+    })
+}
 
-
-
-
-
-
-
+crystalGame()
 
 });
