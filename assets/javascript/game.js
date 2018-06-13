cfea83bc65a909b0eb9633d7ad33e5fcaa1301e0
william zhu <william_zhu@hotmail.ca>
2018-06-13 02:16:48 -0400

    i finially got the score to work

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,37 +1,62 @@
 $(document).ready(function() {
 var wins= 0;
 var loss= 0;
-var goal= 0;
 var score = 0;
 
-
-function crystalGame(result){
-var goal = Math.floor(Math.random() * 120) + 30;
-$(".goal").append(goal);
-console.log(goal);
+var goal = Math.floor(Math.random() * 100) + 30;
+  $(".goal").append(goal);
+  console.log(goal);
 
 
-var numopt1 = Math.floor(Math.random() * 12) + 1; 
-console.log(numopt1);
+function crystalGame(){
 
-var numopt2 = Math.floor(Math.random() * 12) + 1; 
-console.log(numopt2);
+    var numopt1 = Math.floor(Math.random() * 12) + 1; 
+    console.log(numopt1);
 
-var numopt3 = Math.floor(Math.random() * 12) + 1; 
-console.log(numopt3);
+    var numopt2 = Math.floor(Math.random() * 12) + 1; 
+    console.log(numopt2);
 
-var numopt4 = Math.floor(Math.random() * 12) + 1; 
-console.log(numopt4);
+    var numopt3 = Math.floor(Math.random() * 12) + 1; 
+    console.log(numopt3);
 
-$("#crystals1").on("click", function(){
-score = score + numopt;
-console.log(score);
-})
+    var numopt4 = Math.floor(Math.random() * 12) + 1; 
+    console.log(numopt4);
+    
 
-$("#crystals2").on("click", function(){
+    $("#crystals1").on("click", function(){
     score = score + numopt1;
     console.log(score);
+    check()
     })
+
+    $("#crystals2").on("click", function(){
+        score = score + numopt2;
+        console.log(score);
+        check()
+    })
+
+    $("#crystals3").on("click", function(){
+        score = score + numopt3;
+        console.log(score);
+        check()
+    })
+
+    $("#crystals4").on("click", function(){
+        score = score + numopt4;
+        console.log(score);
+        check()
+    })
+}
+
+function check() {
+    
+    if(score === goal){
+        wins++
+        alert("you win")
+        console.log ("you score is " + score);
+        console.log ("your goal is " + goal);
+        console.log (goal);
+    }
 }
 
 crystalGame()
