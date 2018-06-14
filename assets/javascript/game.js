07e991d8aba7043bb68ca4ac5e9b27d6050147c4
william zhu <william_zhu@hotmail.ca>
2018-06-14 19:54:43 -0400

    erased the add the console.logs

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -19,7 +19,6 @@ function crystalGame(){
     goal = 0;
 
     $(".thepoints").html(score);
-
     goal = Math.floor(Math.random() * 100) + 30;
     $(".goal").html(goal);
     console.log(goal);
@@ -28,38 +27,18 @@ function crystalGame(){
     var arr = []
     while(arr.length < 4){
         var randomnumber = Math.floor(Math.random()*12) + 1;
-        //console.log("loop iteration" + randomnumber);
         if(arr.indexOf(randomnumber) > -1) continue;
         arr[arr.length] = randomnumber;
-        //console.log("pushed number " + randomnumber);
     }
 
     numopt1 = parseInt(arr[0]);
-    console.log("this is the ran1 " + numopt1);
 
     numopt2 = parseInt(arr[1]);
-    console.log("this is the ran2 " + numopt2);
 
     numopt3 = parseInt(arr[2]);
-    console.log("this is the ran3 " + numopt3);
 
     numopt4 = parseInt(arr[3]); 
-    console.log("this is the ran3 " + numopt4);
 }
-
-  /*  numopt1 = Math.floor(Math.random() * 12) + 1; 
-    console.log(numopt1);
-
-    numopt2 = Math.floor(Math.random() * 12) + 1; 
-    console.log(numopt2);
-
-    numopt3 = Math.floor(Math.random() * 12) + 1; 
-    console.log(numopt3);
-
-    numopt4 = Math.floor(Math.random() * 12) + 1; 
-    console.log(numopt4);
-}*/
-
 
 function check() {
     $(".thepoints").html(score);
@@ -67,36 +46,38 @@ function check() {
     if(score === goal){
         wins++;
         $(".win").html(wins);
+        $(".hi").html("");
         crystalGame()
     }
     else if(score > goal){
         loss++;
         $(".loss").html(loss);
+        $(".hi").html("");
         crystalGame()
     }
 }
 
 $("#crystals1").on("click", function(){
     score = score + numopt1;
-    console.log(score);
+    $(".crysid1").html("Gem 1: " + numopt1)
     check()
     })
 
     $("#crystals2").on("click", function(){
         score = score + numopt2;
-        console.log(score);
+        $(".crysid2").html("Gem 2: " + numopt2)
         check()
     })
 
     $("#crystals3").on("click", function(){
         score = score + numopt3;
-        console.log(score);
+        $(".crysid3").html("Gem 3: " +  numopt3)
         check()
     })
 
     $("#crystals4").on("click", function(){
         score = score + numopt4;
-        console.log(score);
+        $(".crysid4").html("Gem 4: " + numopt4)
         check()
     })
 
